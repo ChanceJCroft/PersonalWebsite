90f97821004bd6367c1df168e09e83515a065f4f
chance.croft <chance.croft@ruralsourcing.com>
2024-03-14 22:27:22 -0500

    Remove starter scaffolding. Begin layout of new HeaderBar component. Implement Bootstrap for styling.

---
+++ Personal Website/src/components/HeaderBar.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import HeaderTitle from './HeaderTitle.vue'
+</script>
+
+<template>
+  <div class="container text-center">
+    <div class="row">
+      <div class="col-md-3"><img src="../assets/images/orc.jpg" class="img" /></div>
+      <div class="col-md-3"><HeaderTitle /></div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.img {
+  max-width: 25vh;
+  max-height: 40vh;
+}
+</style>
---
+++ Personal Website/src/components/HeaderTitle.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>Chance Croft</h1>
+    <h4>Software Developer</h4>
+  </div>
+</template>
